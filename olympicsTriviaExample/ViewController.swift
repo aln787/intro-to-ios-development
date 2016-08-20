786eba7af485ad79de6ab1a3c59eabbb11271c44
Alex Niderberg <aniderberg@gmail.com>
2016-08-20 18:28:06 -0400

    Show and hide answer label

--- olympicsTriviaExample/ViewController.swift
+++ olympicsTriviaExample/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showAnswer(sender: AnyObject) {
+        answerLabel.hidden = !answerLabel.hidden
+    }
 
 }
 
