03df2a280866c884cef34d22b6e3df15c1aac769
Alex Niderberg <aniderberg@gmail.com>
2016-08-20 18:53:40 -0400

    adding the animation

--- olympicsTriviaExample/ViewController.swift
+++ olympicsTriviaExample/ViewController.swift
@@ -12,10 +12,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
+    var imageView: UIImageView!
+    var screenSize: CGRect = UIScreen.mainScreen().bounds
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let imageName = "gold.png"
+        let image = UIImage(named: imageName)
+        imageView = UIImageView(image: image!)
+        
+        imageView.frame = CGRect(x: (screenSize.width / 2) - 50, y: 0, width: 100, height: 200)
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +33,9 @@ class ViewController: UIViewController {
 
     @IBAction func showAnswer(sender: AnyObject) {
         answerLabel.hidden = !answerLabel.hidden
+        UIView.animateWithDuration(4.0) {
+            self.imageView.frame = CGRect(x: self.imageView.frame.origin.x, y: self.screenSize.height - 100, width: self.imageView.frame.height, height: self.imageView.frame.width)
+        }
     }
 
 }
