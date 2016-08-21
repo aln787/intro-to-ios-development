df92f874c7a8ed6b2eaa36abb1271d3a6400cdae
Alex Niderberg <aniderberg@gmail.com>
2016-08-21 09:17:08 -0400

    initial commit

---
+++ olympicsTriviaExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  olympicsTriviaExample
+//
+//  Created by Niderberg, Alex on 8/20/16.
+//  Copyright © 2016 Niderberg, Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
