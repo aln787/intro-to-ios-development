94d7db772ff8c83d2b44990e862c0bf457bccf89
aln787 <aniderberg@gmail.com>
2020-09-10 00:19:58 -0400

    Preparing for Swift 5 migration

--- olympicsTriviaExample/ViewController.swift
+++ olympicsTriviaExample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     var imageView: UIImageView!
-    var screenSize: CGRect = UIScreen.mainScreen().bounds
+    var screenSize: CGRect = UIScreen.main.bounds
     var questionPairs = [[String:String]]()
     
     override func viewDidLoad() {
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         imageView = UIImageView(image: image!)
         
         imageView.frame = CGRect(x: (screenSize.width / 2) - 50, y: 0, width: 100, height: 200)
-        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
         view.addSubview(imageView)
         
         readJson()
@@ -34,23 +34,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func showAnswer(sender: AnyObject) {
-        answerLabel.hidden = !answerLabel.hidden
-        UIView.animateWithDuration(4.0) {
+    @IBAction func showAnswer2(_ sender: Any) {
+        answerLabel.isHidden = !answerLabel.isHidden
+        UIView.animate(withDuration: 4.0) {
             self.imageView.frame = CGRect(x: self.imageView.frame.origin.x, y: self.screenSize.height - 100, width: self.imageView.frame.height, height: self.imageView.frame.width)
         }
     }
     
     func readJson() {
         //Call this function `readJson()` at the bottom of viewdid load
-        let questionsUrl = NSBundle.mainBundle().URLForResource("triviaQuestions", withExtension: "json")
-        let jsonData = NSData(contentsOfURL: questionsUrl!)
+        let questionsUrl = Bundle.main.url(forResource: "triviaQuestions", withExtension: "json")
+        let jsonData = NSData(contentsOf: questionsUrl!)
         
         do{
-            let jsonObject = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: .AllowFragments)
+            let jsonObject = try JSONSerialization.jsonObject(with: jsonData! as Data, options: .allowFragments)
             if let jsonDict = jsonObject as? [String: AnyObject] {
                 //Add an empty class variable `var questionPairs = [[String:String]]()` at the top of this class
-                questionPairs = parseRelevantJson(jsonDict)
+                questionPairs = parseRelevantJson(jsonObject: jsonDict)
                 print(questionPairs)
             }
         } catch {
@@ -74,10 +74,9 @@ class ViewController: UIViewController {
         }
         return triviaPairs
     }
-
     
-    @IBAction func nextQuestion(sender: AnyObject) {
-        updateQuestionPair(questionPairs)
+    @IBAction func NextQuestion(_ sender: Any) {
+        updateQuestionPair(fullQuestionList: questionPairs)
     }
     
     func updateQuestionPair(fullQuestionList: [Dictionary<String, String>]) {
@@ -85,8 +84,8 @@ class ViewController: UIViewController {
         let random = Int(arc4random_uniform(numberOfQuestions))
         let nextQuestion = fullQuestionList[random]
         questionLabel.text = nextQuestion["question"]
-        answerLabel.hidden = true
         answerLabel.text = nextQuestion["answer"]
+        answerLabel.isHidden = !answerLabel.isHidden
     }
 
 
